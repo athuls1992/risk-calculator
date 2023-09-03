ee341e9a037657cf69baaa50a571568a557ddc08
Athul Sreenivasan <52868837+athuls1992@users.noreply.github.com>
2023-09-03 14:47:02 +0300

    Update main.js
    2023 updates

--- main.js
+++ main.js
@@ -1,39 +1,39 @@
-const loanAmountInput = document.querySelector(".loan-amount");
+document.getElementById('myForm').reset();
+const annualRevenueInput = document.querySelector(".annual-rev");
 const interestRateInput = document.querySelector('#industry');
-const loanTenureInput = document.querySelector('#employee');
+const empCountInput = document.querySelector('#employee');
 
-// const avgCost = document.querySelector(".loan-emi .value");
-// const finRisk = document.querySelector(".total-interest .value");
-
-const loanEMIValue = document.querySelector(".loan-emi .value");
+const avgCostValue = document.querySelector(".avg-cost .value");
 const totalInterestValue = document.querySelector(".total-interest .value");
 const totalAmountValue = document.querySelector(".total-amount .value ");
 
 const calculateBtn = document.querySelector(".calculate-btn");
 
-// let annualRevenue = parseFloat(loanAmountInput.value);
-// let busType = parseFloat(interestRateInput.value);
-
-let loanAmount = parseFloat(loanAmountInput.value);
+let annualRevenue = parseFloat(annualRevenueInput.value);
 let interestRate = parseFloat(interestRateInput.value);
-let loanTenure = parseFloat(loanTenureInput.value);
+let empCount = parseFloat(empCountInput.value);
+let avgCost = parseFloat(empCountInput.value);
 
-let interest = interestRate / 12 / 100;
+const riskRatio = 0.59;
 
 let myChart;
 
 const checkValues = () => {
-  let loanAmountValue = loanAmountInput.value;
+  let annualRevenueValue = annualRevenueInput.value;
   let interestRateValue = interestRateInput.value;
-  let loanTenureValue = loanTenureInput.value;
+  let empCountValue = empCountInput.value;
+
+  // if (this.annualRevenueInput === '0') {
+  //   document.getElementById('emivalue').innerHTML = '';
+  //   }
 
   let regexNumber = /^[0-9]+$/;
-  if (!loanAmountValue.match(regexNumber)) {
-    loanAmountInput.value = "10000";
+  if (!annualRevenueValue.match(regexNumber)) {
+    annualRevenueInput.value = "";
   }
 
-  if (!loanTenureValue.match(regexNumber)) {
-    loanTenureInput.value = "12";
+  if (!empCountValue.match(regexNumber)) {
+    empCountInput.value = "12";
   }
 
   let regexDecimalNumber = /^(\d*\.)?\d+$/;
@@ -45,54 +45,62 @@ const checkValues = () => {
 const displayChart = (totalInterestPayableValue) => {
   const ctx = document.getElementById("myChart").getContext("2d");
   myChart = new Chart(ctx, {
-    type: "pie",
+    type: "bar",
     data: {
-      labels: ["Avg. Risk", "Compared to industry"],
+      labels: ["Financial Risk", "Cost based on industry"],
       datasets: [
         {
-          data: [totalInterestPayableValue, loanAmount],
+          label: ["Financial Risk"],
+          data: [totalInterestPayableValue,avgCost],
           backgroundColor: ["#e63946", "#040404"],
           borderWidth: 0,
         },
       ],
     },
+    // options: {
+    //   scales: {
+    //     x: {
+    //       stacked: true
+    //   },
+    //   y: {
+    //       stacked: true
+    //   }
+    //   }
+    // },
   });
 };
 
 const updateChart = (totalInterestPayableValue) => {
   myChart.data.datasets[0].data[0] = totalInterestPayableValue;
-  myChart.data.datasets[0].data[1] = loanAmount;
+  myChart.data.datasets[0].data[1] = avgCost;
   myChart.update();
 };
 
 const refreshInputValues = () => {
-  loanAmount = parseFloat(loanAmountInput.value);
+  annualRevenue = parseFloat(annualRevenueInput.value);
   interestRate = parseFloat(interestRateInput.value);
-  loanTenure = parseFloat(loanTenureInput.value);
-  interest = interestRate / 12 / 100;
+  empCount = parseFloat(empCountInput.value);
+  interest = interestRate * (empCount/100);
 };
-
 const calculateEMI = () => {
   checkValues();
   refreshInputValues();
-  let emi =
-    loanAmount *
-    interest *
-    (Math.pow(1 + interest, loanTenure) /
-      (Math.pow(1 + interest, loanTenure) - 1));
-
+  
+  let employeeRatio = empCount/100;
+  let emi = interestRate * employeeRatio;
+  // console.log(emi);
   return emi;
 };
 
 const updateData = (emi) => {
-  loanEMIValue.innerHTML = Math.round(emi);
+  avgCost = Math.round(emi)
+  avgCostValue.innerHTML = Math.round(emi).toLocaleString();
 
-  let totalAmount = Math.round(loanTenure * emi);
-  totalAmountValue.innerHTML = totalAmount;
+  let totalAmount = Math.round(empCount * emi);
+  totalAmountValue.innerHTML = totalAmount.toLocaleString();
 
-  let totalInterestPayable = Math.round(totalAmount - loanAmount);
-  totalInterestValue.innerHTML = totalInterestPayable;
-
+  let totalInterestPayable = Math.round(emi * riskRatio);
+  totalInterestValue.innerHTML = totalInterestPayable.toLocaleString();
   if (myChart) {
     updateChart(totalInterestPayable);
   } else {
@@ -101,10 +109,58 @@ const updateData = (emi) => {
 };
 
 const init = () => {
-  let emi = calculateEMI();
+  var emi = calculateEMI();
   updateData(emi);
 };
 
 init();
 
+
+// var calculatedEMI = document.getElementById('emivalue').innerHTML;
+
 calculateBtn.addEventListener("click", init);
+
+function sendIt() {
+  var endpoint = 'https://eoa8in2k3yelhfr.m.pipedream.net';
+var formData = {
+  domain_name: $(".domain-type").val(),
+  industry: $("#industry").find(":selected").text(),
+  revenue: $(".annual-rev").val(),
+  empCapacity: $("#employee").find(":selected").text(),
+}
+  $.ajax({
+      type: "POST",
+      url: endpoint,
+      data: formData,
+      dataType: "json"
+  });
+}
+
+function validateForm() {
+  let x = document.forms["myForm"]["field"].value;
+  if (x == "") {
+    alert("website address must be filled out");
+    return false;
+  }
+}
+
+jQuery.validator.setDefaults({
+  debug: true,
+  success: "valid"
+});
+$( "#myform" ).validate({
+  rules: {
+    field: {
+      required: true,
+      url: true,
+    }
+  }
+});
+
+function ValidURL(str) {
+  var regex = /^((ftp|http|https):\/\/)?(www.)?(?!.*(ftp|http|https|www.))[a-zA-Z0-9_-]+(\.[a-zA-Z]+)+((\/)[\w#]+)*(\/\w+\?[a-zA-Z0-9_]+=\w+(&[a-zA-Z0-9_]+=\w+)*)?\/?$/gm;
+  if (!regex.test(str)|| (str.length <= 3)) {
+      alert("Please enter valid URL.");
+      return false;
+  }
+}
